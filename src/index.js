c780f8839cdb3aaf3db3efdd2530cdec384df1a1
Michael Haar <michael.haar92@gmail.com>
2024-04-18 15:20:26 +0200

    minor bugfix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { run: jscodeshift } = require('jscodeshift/src/Runner');
 const path = require('node:path');
 
 const transformPath = path.resolve('src/transform.js');
-const paths = ['__testfixtures__/sample-1.test.js'];
+const paths = ['src/__testfixtures__/sample-1.input.js'];
 const options = {
   dry: true,
   print: true,
