a611a5bffb811e998718bfecf1b67bbb152434ed
Michael Haar <michael.haar92@gmail.com>
2024-04-18 14:03:18 +0200

    minor DX improvements

--- src/transform.js
+++ src/transform.js
@@ -1,8 +1,11 @@
+const j = require("jscodeshift");
+
 module.exports = function (fileInfo, api, options) {
   // transform `fileInfo.source` here
 
-  // e.g. convert all `foo` function names to `bar`
-  const changedSource = fileInfo.source.replace(/foo/g, "bar");
+  const root = j(fileInfo.source);
+
+  const changedSource = root.findVariableDeclarators("foo").renameTo("bar").toSource();
 
   // return the updated source code
   return changedSource;
---
+++ test/example2.js
@@ -0,0 +1,3 @@
+const foo = 42;
+
+const favoriteFruit = "apple";
