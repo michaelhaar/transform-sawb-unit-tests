557df15118287ef83a6c592a13838db7268983fc
Michael Haar <michael.haar92@gmail.com>
2024-04-18 15:05:47 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { run: jscodeshift } = require('jscodeshift/src/Runner');
 const path = require('node:path');
 
 const transformPath = path.resolve('src/transform.js');
-const paths = ['test/example2.js'];
+const paths = ['__testfixtures__/sample-1.test.js'];
 const options = {
   dry: true,
   print: true,
