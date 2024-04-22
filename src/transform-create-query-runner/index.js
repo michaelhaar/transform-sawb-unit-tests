d8d3d2b1e2173a5717c350844bc745bd1057a385
Michael Haar <michael.haar92@gmail.com>
2024-04-22 15:19:07 +0200

    first draft

---
+++ src/transform-create-query-runner/index.js
@@ -0,0 +1,13 @@
+const { run: jscodeshift } = require('jscodeshift/src/Runner');
+const path = require('node:path');
+const { globSync } = require('glob');
+
+const transformPath = path.resolve('src/transform-create-query-runner/transform.js');
+const paths = globSync('/Users/michael/dev/sawb/wb-core/test/**/*.test.js');
+const options = {
+  dry: false,
+  print: false,
+  verbose: 1,
+};
+
+jscodeshift(transformPath, paths, options);
--- src/transform-create-query-runner/transform.js
+++ src/transform-create-query-runner/transform.js
@@ -5,5 +5,45 @@ module.exports = function (fileInfo, api, options) {
 
   const root = j(fileInfo.source);
 
+  // find all `const runner = createQueryRunner(t, [` variable declarations
+  root.find(j.VariableDeclaration).forEach((runnerDeclarationPath) => {
+    if (
+      runnerDeclarationPath.value.declarations.length !== 1 ||
+      runnerDeclarationPath.value.declarations[0].id.name !== 'runner'
+    ) {
+      return;
+    }
+
+    const callExpressionCollection = j(runnerDeclarationPath).find(j.CallExpression);
+
+    if (callExpressionCollection.length === 0) {
+      return;
+    }
+
+    const callExpressionPath = callExpressionCollection.paths()[0];
+
+    if (callExpressionPath.value.callee.name !== 'createQueryRunner') {
+      throw new Error('Invalid `call` expression found...');
+    }
+
+    const arrayExpression = callExpressionPath.value.arguments[1];
+
+    // loop through all elements in the array expression
+    arrayExpression.elements.forEach((element) => {
+      if (element.type === 'ArrowFunctionExpression') {
+        // find return statement
+        const returnStatementCollection = j(element.body).find(j.ReturnStatement);
+        if (returnStatementCollection.length === 0) {
+          // return query.response([]);
+          element.body = j.blockStatement([
+            j.returnStatement(j.callExpression(j.identifier('query.response'), [j.arrayExpression([])])),
+          ]);
+        } else {
+          element.body = j.blockStatement([returnStatementCollection.paths()[0].value]);
+        }
+      }
+    });
+  });
+
   return root.toSource(); // return the updated source code
 };
