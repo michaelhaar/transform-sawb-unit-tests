436d411a9e08b297f4049fe62d26a00fd8880c82
Michael Haar <michael.haar92@gmail.com>
2024-04-22 15:18:13 +0200

    bugfix

--- src/transform-switch-statements/index.js
+++ src/transform-switch-statements/index.js
@@ -2,7 +2,7 @@ const { run: jscodeshift } = require('jscodeshift/src/Runner');
 const path = require('node:path');
 const { globSync } = require('glob');
 
-const transformPath = path.resolve('src/transform.js');
+const transformPath = path.resolve('src/transform-switch-statements/transform.js');
 const paths = globSync('/Users/michael/dev/sawb/wb-core/test/**/*.test.js');
 const options = {
   dry: false,
